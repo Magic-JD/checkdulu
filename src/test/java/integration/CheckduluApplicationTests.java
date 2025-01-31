67af6b8b169070f9759e752699379a92c98e1980
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-01-31 21:07:42 +0800

    Moving integration tests to a seperate folder

---
+++ src/test/java/integration/CheckduluApplicationTests.java
@@ -0,0 +1,17 @@
+package integration;
+
+import com.example.checkdulu.CheckduluApplication;
+import com.example.checkdulu.exception.CheckDuluException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+		classes = {CheckduluApplication.class}
+)
+class CheckduluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
