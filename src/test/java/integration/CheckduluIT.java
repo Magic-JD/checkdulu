669b0801b5c34c26292bd8ec4ee97b9b20189269
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-01-31 21:12:51 +0800

    Setting tests to just run the unit tests and verify to run both unit and integration tests.

--- src/test/java/integration/CheckduluIT.java
+++ src/test/java/integration/CheckduluIT.java
@@ -1,14 +1,13 @@
 package integration;
 
 import com.example.checkdulu.CheckduluApplication;
-import com.example.checkdulu.exception.CheckDuluException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
 		classes = {CheckduluApplication.class}
 )
-class CheckduluApplicationTests {
+class CheckduluIT {
 
 	@Test
 	void contextLoads() {
