06b4dae9c85420cc9892efd1c030f5e96adf275c
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2025-02-06 18:15:46 +0800

    Adding an endpoint for the mobile app to connect to

--- src/main/java/com/example/checkdulu/config/Config.java
+++ src/main/java/com/example/checkdulu/config/Config.java
@@ -2,15 +2,23 @@ package com.example.checkdulu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpClient;
 
 @Configuration
 public class Config {
     @Bean
-    public HttpClient httpClient() {
+    HttpClient httpClient() {
         return HttpClient
                 .newBuilder()
                 .build();
+    }
+        @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
--- src/main/java/com/example/checkdulu/controller/InfoController.java
+++ src/main/java/com/example/checkdulu/controller/InfoController.java
@@ -2,16 +2,19 @@ package com.example.checkdulu.controller;
 
 import com.example.checkdulu.service.InfoService;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("info")
+@Slf4j
 public class InfoController {
 
     private final InfoService infoService;
@@ -29,4 +32,15 @@ public class InfoController {
             return "product";
         }).orElse("none");
     }
+
+    @GetMapping("mobile")
+    public ResponseEntity<InformationResponse> getInformationMobile(@RequestParam("barcode") String barcode, @RequestParam(value = "amount") Optional<Double> amount){
+        return infoService.getInfo(barcode).map(productInfo ->
+             ResponseEntity.ok(new InformationResponse(productInfo.name(), productInfo.sugarPerXg(amount.orElse(100d))))).orElseThrow(() -> {
+                 log.error(STR."Product could not be found \{barcode}");
+                 return new RuntimeException();
+        });
+    }
+
+    private record InformationResponse(String productName, double sugarPerGivenAmount){ }
 }
